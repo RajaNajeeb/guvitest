2694b0d1b6ec3d2c324e8aa972b2ecd7c2d07b44
Raja Najeeb <rajanajeeb34@gmail.com>
2016-09-12 15:26:13 +0530

    test7.c

---
+++ test7.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int x;
+scanf("%d",&x);
+if((x=='a')||(x=='e')||(x=='i')||(x=='o')||(x=='u')||(x=='A')||(x=='E')||(x=='I')||(x=='O')||(x=='U'))
+{
+printf("Vowel");
+}
+else if((x>='b'&& x<='z')||(x>='B'&& x<='Z'))
+{
+printf("Consonant");
+}
+else
+{
+printf("Not an alphabet");
+}
+return 0;
+}
